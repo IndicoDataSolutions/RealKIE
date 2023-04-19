5a7ffde90784a11b60f00ed9f56179674c5b8d89
benleetownsend <benlt@hotmail.co.uk>
2023-04-19 14:56:39 +0100

    FIX: re-add full set of models to sweep script

--- scripts/run_sweeps.sh
+++ scripts/run_sweeps.sh
@@ -8,7 +8,7 @@ do
         docker-compose exec finetune python3 train_and_predict.py wandb_sweep $PROJECT $ENTITY $dataset $finetune_base_model
     done
 
-    for hf_base_model in distilbert-base-uncased # longformer-base-4096 deberta-v3-base deberta-v3-large
+    for hf_base_model in distilbert-base-uncased longformer-base-4096 deberta-v3-base deberta-v3-large
     do
         docker-compose exec huggingface python3 train_and_predict.py wandb_sweep $PROJECT $ENTITY $dataset $hf_base_model
     done
