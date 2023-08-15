24ed4cc5d6f0b1b6109173874fb7520cc993b9ba
benleetownsend <benlt@hotmail.co.uk>
2023-08-15 16:24:52 +0100

    FIX: filter completed datasets

--- finetune/run_sweeps.sh
+++ finetune/run_sweeps.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 ./get_data.sh
 
-for dataset in s1_truncated fcc_invoices # charities nda resource_contracts
+for dataset in fcc_invoices # charities nda resource_contracts
 do
-    for finetune_base_model in xdoc roberta-base
+    for finetune_base_model in roberta-base
+    do
+        python3 train_and_predict.py wandb_sweep $PROJECT $ENTITY $dataset $finetune_base_model
+    done
+done
+
+for dataset in s1_truncated # charities nda resource_contracts
+do
+    for finetune_base_model in roberta-base xdoc
     do
         python3 train_and_predict.py wandb_sweep $PROJECT $ENTITY $dataset $finetune_base_model
     done
--- layoutlmv3/run_sweeps.sh
+++ layoutlmv3/run_sweeps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./get_data.sh
 
-for dataset in resource_contracts s1_truncated fcc_invoices # charities nda 
+for dataset in s1_truncated # resource_contracts fcc_invoices # charities nda 
 do
     python3 train_and_predict.py wandb_sweep $PROJECT $ENTITY $dataset
 done
