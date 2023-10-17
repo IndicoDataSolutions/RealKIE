3e74e70707ece0cba8115d4e4450495d0f09af98
Christopher Wells <chris.wells@indico.io>
2023-10-17 15:36:31 -0400

    addressing BLT's comments.

--- finetune/finetune_helpers.py
+++ finetune/finetune_helpers.py
@@ -1,3 +1,9 @@
+import os
+import gzip
+import json
+
+from helpers import fix_page_offsets, strip_unused_ocr_data
+
 from finetune.base_models import (
     BERTLarge,
     BERTModelCased,
@@ -43,3 +49,19 @@ def get_base_model(base_model):
             "is_document": False,
         },
     }[base_model]
+
+
+def get_model_input_from_csv(csv, is_document, dataset_dir):
+    if is_document:
+        ocr = []
+        for ocr_file, text in zip(csv.ocr, csv.text):
+            ocr_file = os.path.join(dataset_dir, ocr_file)
+            with gzip.open(ocr_file, "rt") as fp:
+                doc_ocr = json.loads(fp.read())
+                doc_ocr = fix_page_offsets(
+                    [strip_unused_ocr_data(ocr_page) for ocr_page in doc_ocr]
+                )
+                ocr.append(doc_ocr)
+            assert "\n".join(page["pages"][0]["text"] for page in ocr[-1]) == text
+        return ocr
+    return csv.text
